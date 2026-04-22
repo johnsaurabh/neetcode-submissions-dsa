0adf8cfa1b721042a929828358b40d1f30d3b909
John Saurabh <41294425+johnsaurabh@users.noreply.github.com>
2026-04-22 11:02:50 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        
+        hmap=defaultdict(list)
+
+        for word in strs:
+            count=[0]*26
+            for ch in word:
+                count[ord(ch)-97]+=1
+
+            hmap[tuple(count)].append(word)
+
+        return list(hmap.values())
+        
