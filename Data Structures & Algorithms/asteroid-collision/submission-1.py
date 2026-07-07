a3cf457d445f938f6e045030e18cb56d209d79a7
John Saurabh <41294425+johnsaurabh@users.noreply.github.com>
2026-07-06 18:51:23 -0700

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack=[]
+
+        for ast in asteroids:
+            destroyed=False
+            
+            while stack and ast<0 and stack[-1]>0:
+                if abs(ast)< abs(stack[-1]):
+                    destroyed=True
+                    break
+                elif abs(ast)>abs(stack[-1]):
+                    stack.pop()
+                else:
+                    stack.pop()
+                    destroyed=True
+                    break
+            if not destroyed:
+                stack.append(ast)
+        return stack
+                
+
+        
